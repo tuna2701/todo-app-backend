e67235ef1c62aee5c083831ac0238ae3591940bc
tuna2701 <108262239+tuna2701@users.noreply.github.com>
2022-09-17 16:03:07 +0700

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "todo-app",
+  HOST: "sql206.epizy.com",
+  USER: "epiz_32611496",
+  PASSWORD: "oUOyswJbqbYPpy",
+  DB: "epiz_32611496_todoapp",
 };
