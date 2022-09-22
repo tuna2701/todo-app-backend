2c6e8c3485f90b2c39d0b8126a9a40f2d3d3ebd5
tuna2701 <108262239+tuna2701@users.noreply.github.com>
2022-09-22 15:14:23 +0700

    commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,26 @@
+//https://www.freesqldatabase.com/account/
+// module.exports = {
+//   HOST: "sql6.freesqldatabase.com",
+//   USER: "sql6521452",
+//   PASSWORD: "tgtD8aZBw2",
+//   DB: "sql6521452",
+//   PORT: 3306,
+// };
+
+//https://www.000webhost.com
+// module.exports = {
+//   HOST: "sql6.freesqldatabase.com",
+//   USER: "id19605742_natuan0207",
+//   PASSWORD: "Natuan_020701",
+//   DB: "id19605742_todo_app",
+//   PORT: 3306,
+// };
+
+//https://remotemysql.com/databases.php
 module.exports = {
-  HOST: "sql206.epizy.com",
-  USER: "epiz_32611496",
-  PASSWORD: "oUOyswJbqbYPpy",
-  DB: "epiz_32611496_todoapp",
+  HOST: "remotemysql.com",
+  USER: "xv86yb0Z4W",
+  PASSWORD: "ddLSt48dP1",
+  DB: "xv86yb0Z4W",
+  PORT: 3306,
 };
